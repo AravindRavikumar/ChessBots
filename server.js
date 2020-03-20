0b2d980d4db2e6ea86b22feb4c380a6ba8745ad9
AravindRavikumar <akgr421@gmail.com>
2020-03-20 23:32:57 +0530

    Update server.js to call minmax.py

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var path = require('path');
 var bodyParser = require("body-parser");
+var spawn = require("child_process").spawn;
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
@@ -15,14 +16,27 @@ app.get('/index.html', function (req, res) {
 });
 
 app.get('/bot1.html', function (req, res) {
+	console.log("Bot1 - GET")
 	res.sendFile(path.join(__dirname,"views/bot1.html"));
 });
 
 app.post('/curpos',function (req, res){
-	console.log(req.body);
+	var boardFEN = req.body['game'];
+	//console.log(boardFEN);
+	var pythonProcess = spawn('C:/Users/Aravind Ravikumar/AppData/Local/Programs/Python/Python37/python.exe',["public/chess_engine/minmax.py",boardFEN]);		//Because path not detected in python script, use this if Windows
+
+	pythonProcess.stdout.on('data', function(data){
+		console.log(data.toString());
+	 });
+
+	 pythonProcess.stderr.on('data', function(data){
+		console.log("Error in script");
+		console.log(data.toString());
+	 });
+
 });
 
- var server = app.listen(3000, "10.250.7.145", function () {
+ var server = app.listen(8080, "127.0.0.1", function () {
 	var host = server.address().address;
 	var port = server.address().port;
  
