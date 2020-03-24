42c84f42314bb1df5565172243ff2c7621d6889d
AravindRavikumar <akgr421@gmail.com>
2020-03-24 16:24:05 +0530

    Position based value of pieces

--- public/chess_engine/minimax.py
+++ public/chess_engine/minimax.py
@@ -2,6 +2,98 @@ import sys
 import chess
 import random
 
+#check https://www.chessprogramming.org/Simplified_Evaluation_Function for position values
+bPawnPosValue = [0,  0,  0,  0,  0,  0,  0,  0,
+				50, 50, 50, 50, 50, 50, 50, 50,
+				10, 10, 20, 30, 30, 20, 10, 10,
+				5,  5, 10, 25, 25, 10,  5,  5,
+				0,  0,  0, 20, 20,  0,  0,  0,
+				5, -5,-10,  0,  0,-10, -5,  5,
+				5, 10, 10,-20,-20, 10, 10,  5,
+				0,  0,  0,  0,  0,  0,  0,  0]
+
+wPawnPosValue = [0,  0,  0,  0,  0,  0,  0,  0,
+				5, 10, 10,-20,-20, 10, 10,  5,
+				5, -5,-10,  0,  0,-10, -5,  5,
+				0,  0,  0, 20, 20,  0,  0,  0,
+				5,  5, 10, 25, 25, 10,  5,  5,
+				10, 10, 20, 30, 30, 20, 10, 10,
+				50, 50, 50, 50, 50, 50, 50, 50,
+				0,  0,  0,  0,  0,  0,  0,  0]
+
+bKnightPosValue = [-50,-40,-30,-30,-30,-30,-40,-50,
+				-40,-20,  0,  0,  0,  0,-20,-40,
+				-30,  5, 15, 20, 20, 15,  5,-30,
+				-30,  0, 15, 20, 20, 15,  0,-30,
+				-30,  5, 10, 15, 15, 10,  5,-30,
+				-30,  0, 10, 15, 15, 10,  0,-30,
+				-40,-20,  0,  5,  5,  0,-20,-40,
+				-50,-40,-30,-30,-30,-30,-40,-50]
+
+wKnightPosValue = [-50,-40,-30,-30,-30,-30,-40,-50,
+				-40,-20,  0,  5,  5,  0,-20,-40,
+				-30,  0, 10, 15, 15, 10,  0,-30,
+				-30,  5, 10, 15, 15, 10,  5,-30,
+				-30,  0, 15, 20, 20, 15,  0,-30,
+				-30,  5, 15, 20, 20, 15,  5,-30,
+				-40,-20,  0,  0,  0,  0,-20,-40,
+				-50,-40,-30,-30,-30,-30,-40,-50]
+
+bBishopPosValue = [-20,-10,-10,-10,-10,-10,-10,-20,
+				-10,  5,  0,  0,  0,  0,  5,-10,
+				-10, 10, 10, 10, 10, 10, 10,-10,
+				-10,  0, 10, 10, 10, 10,  0,-10,
+				-10,  5,  5, 10, 10,  5,  5,-10,
+				-10,  0,  5, 10, 10,  5,  0,-10,
+				-10,  0,  0,  0,  0,  0,  0,-10,
+				-20,-10,-10,-10,-10,-10,-10,-20]
+
+wBishopPosValue = [-20,-10,-10,-10,-10,-10,-10,-20,
+				-10,  0,  0,  0,  0,  0,  0,-10,
+				-10,  0,  5, 10, 10,  5,  0,-10,
+				-10,  5,  5, 10, 10,  5,  5,-10,
+				-10,  0, 10, 10, 10, 10,  0,-10,
+				-10, 10, 10, 10, 10, 10, 10,-10,
+				-10,  5,  0,  0,  0,  0,  5,-10,
+				-20,-10,-10,-10,-10,-10,-10,-20]
+
+bRookPosValue = [ 0,  0,  0,  0,  0,  0,  0,  0,
+				5, 10, 10, 10, 10, 10, 10,  5,
+				-5,  0,  0,  0,  0,  0,  0, -5,
+				-5,  0,  0,  0,  0,  0,  0, -5,
+				-5,  0,  0,  0,  0,  0,  0, -5,
+				-5,  0,  0,  0,  0,  0,  0, -5,
+				-5,  0,  0,  0,  0,  0,  0, -5,
+				0,  0,  0,  5,  5,  0,  0,  0]
+
+wRookPosValue = [0,  0,  0,  5,  5,  0,  0,  0,
+				-5,  0,  0,  0,  0,  0,  0, -5,
+				-5,  0,  0,  0,  0,  0,  0, -5,
+				-5,  0,  0,  0,  0,  0,  0, -5,
+				-5,  0,  0,  0,  0,  0,  0, -5,
+				-5,  0,  0,  0,  0,  0,  0, -5,
+				5, 10, 10, 10, 10, 10, 10,  5,
+				0,  0,  0,  0,  0,  0,  0,  0]
+
+bQueenPosValue = [-20,-10,-10, -5, -5,-10,-10,-20,
+			-10,  0,  0,  0,  0,  0,  0,-10,
+			-10,  0,  5,  5,  5,  5,  0,-10,
+			-5,  0,  5,  5,  5,  5,  0, -5,
+			0,  0,  5,  5,  5,  5,  0, -5,
+			-10,  5,  5,  5,  5,  5,  0,-10,
+			-10,  0,  5,  0,  0,  0,  0,-10,
+			-20,-10,-10, -5, -5,-10,-10,-20]
+
+wQueenPosValue = [-20,-10,-10, -5, -5,-10,-10,-20,
+				-10,  0,  5,  0,  0,  0,  0,-10,
+				-10,  5,  5,  5,  5,  5,  0,-10,
+				0,  0,  5,  5,  5,  5,  0, -5,
+				-5,  0,  5,  5,  5,  5,  0, -5,
+				-10,  0,  5,  5,  5,  5,  0,-10,
+				-10,  0,  0,  0,  0,  0,  0,-10,
+				-10,  0,  0,  0,  0,  0,  0,-10,
+				-20,-10,-10, -5, -5,-10,-10,-20]
+
 def minmaxBest(depth, board):
 	moveList = board.legal_moves
 	valBest = 10000
@@ -43,33 +135,59 @@ def minmax(depth, board, isMax):
 def evaluate(board):
 	boardEval = 0
 	for i in range(0,64):
-		boardEval += getValue(str(board.piece_at(i)))
+		#print(i,end=" ")
+		boardEval += getValue(str(board.piece_at(i))) + posFactor(str(board.piece_at(i)),i)
 		#print(board.piece_at(i))
 	return boardEval
 
-def getValue(piece):
+def posFactor(piece,pos):
+	return 0
+	if piece == "p":
+		return -bPawnPosValue[pos]
+	elif piece == "P":
+		return wPawnPosValue[pos]
+	elif piece == "n":
+		return -bKnightPosValue[pos]
+	elif piece == "N":
+		return wKnightPosValue[pos]
+	elif piece == "b":
+		return -bBishopPosValue[pos]
+	elif piece == "B":
+		return wBishopPosValue[pos]
+	elif piece == "q":
+		return -bQueenPosValue[pos]
+	elif piece == "Q":
+		return wQueenPosValue[pos]
+	else:
+		return 0
+
+def getValue(piece): 		#check-out https://www.chessprogramming.org/Simplified_Evaluation_Function
 	if piece == None:
 		return 0
 	elif piece == "p":
-		return -1
-	elif piece == "P":
-		return 1
-	elif piece == "n" or piece == "b":
-		return -3
-	elif piece == "N" or piece == "B":
-		return 3
-	elif piece == "r":
-		return -5
-	elif piece == "R":
-		return 5
-	elif piece == "q":
-		return -9
-	elif piece == "Q":
-		return 9
-	elif piece == "k":
 		return -100
-	elif piece == "K":
+	elif piece == "P":
 		return 100
+	elif piece == "n":
+		return -320
+	elif piece == "N":
+		return 320
+	elif piece == "b":
+		return -330	
+	elif piece == "B":
+		return -330		
+	elif piece == "r":
+		return -500
+	elif piece == "R":
+		return 500
+	elif piece == "q":
+		return -900
+	elif piece == "Q":
+		return 900
+	elif piece == "k":
+		return -20000
+	elif piece == "K":
+		return 20000
 	else:
 		return 0
 
@@ -85,6 +203,8 @@ legal_moves = [x for x in board.legal_moves] 	#check https://github.com/niklasf/
 
 #randMoveIndex = random.randint(0,len(legal_moves)-1)
 bestMove = minmaxBest(3,board)
+
+#print(evaluate(board))
 
 #print(legal_moves[randMoveIndex])	
 print(bestMove)
